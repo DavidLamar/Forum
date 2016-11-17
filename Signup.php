1ba9ca481d0a6ae93c4800d1e94ae37ec97fd907
marshalb <marshalb@mail.gvsu.edu>
2016-11-16 22:43:52 -0500

    Signup verification drafted

--- Signup.php
+++ Signup.php
@@ -15,10 +15,28 @@
 				</div>
 			</div>
 			<div>
-				<button type="submit">Create Account</button>
+				<button type="submit" onclick="return verify()">Create Account</button>
 			</div>
 		</form>
 
 	</body>
+	<script>
+	//ADD VERIFICATION
+		function verify(){
+			var pass = document.getElementById('login').elements[1].value;
+			var special = pass.search(/[!@#$%^&*()]/g);
+			var numeral = pass.search(/[0-9]/g);
+			/*Password must be greater than 5 characters, with at least
+			one special character and one number. Here we must use regular
+			expressions to check the input.*/
+			if (special == -1 || numeral == -1 || pass.length < 6) { 
+				alert("Password must be longer than 5 characters and must "+
+				"contain at least one numeral and one special character.");
+				return false;
+			} else {
+				return true;
+			}
+		}
+	</script>
 
 </html>
