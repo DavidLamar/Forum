8c3fc96fa3026dda6a288513a78e1ef04062f1aa
cunninro <cunninro@mail.gvsu.edu>
2016-11-21 14:05:05 -0500

    Add files via upload
    added classes and ids

--- Login.php
+++ Login.php
@@ -1,23 +1,27 @@
 <html>
-
+<head><link href="forum.css" rel="stylesheet" type="text/css"></head>
 	<body>
 	<?php include "Header.php";?>
+	<div class = "wrapper">
+	<h1>SUPER COOL TITLE</h1>
 	<form method="POST" id="login" action="/~lamard/Forum/scripts/Login.php">
 		<div>
-			<div>
+			<div id = "username">
 				Username: <input type="text" name="username" />
 			</div>
-			<div>
+			<div id ="password">
 				Password: <input type="password" name="password" />
 			</div>
 		</div>
 		<div>
-			<button type="submit">Login</button>
+			<button type="submit" class ="login loginEffect" >Login</button>
 		</div>
+		
 		<div>
 			Not a user? <a href="Signup.php">Sign up</a>
-		</div>
+		</div> 
 	</form>
+	</div>
 
 	</body>
 
