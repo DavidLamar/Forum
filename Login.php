c9522c59ffcf8f18ac65996e985a00e8144dd69d
cunninro <cunninro@mail.gvsu.edu>
2016-12-03 23:26:04 -0500

    Add files via upload
    fixed some CSS, added "Login" title, removed weird button transition on page load, added responsive

--- Login.php
+++ Login.php
@@ -1,29 +1,23 @@
 <html>
-<head><link href="forum.css" rel="stylesheet" type="text/css"></head>
+	<head>
+		<title>Login</title>
+		<link href="forum.css" rel="stylesheet" type="text/css">
+	</head>
+	
 	<body>
-	<?php include "Header.php";?>
-	<div class = "wrapper">
-	<h1>SUPER COOL TITLE</h1>
-	<form method="POST" id="login" action="/~lamard/Forum/scripts/Login.php">
-		<div>
-			<div id = "username">
-				Username: <input type="text" name="username" />
-			</div>
-			<div id ="password">
-				Password: <input type="password" name="password" />
-			</div>
-		</div>
-		<div>
-			<button type="submit" class ="login loginEffect" >Login</button>
-		</div>
-		
-		<div>
-			Not a user? <a href="Signup.php">Sign up</a>
-		</div> 
-	</form>
-	</div>
+		<?php include "Header.php";?>
+		<div class = "wrapper_login">
+			<h1>CIS 371 Forum</h1>
+			<form method="POST" id="login" action="/~lamard/Forum/scripts/Login.php">
 
+				<div class = "form_field"> <label>Username:</label> <input type="text" name="username" /> </div> 
+				<div class = "form_field"> <label>Password:</label> <input type="password" name="password" /> </div> 
+
+				<div><button type="submit" class ="login loginEffect" >Login</button></div>
+				<br/>
+					
+				<footer>Not a user? <a href="Signup.php">Sign up</a></footer> 
+			</form>
+		</div>
 	</body>
-
-
 </html>
